f19c4e27205113ae794b867e97cc6783ecbb5ef3
Stakedell3 <Stakedell3@192.168.108.94>
2017-06-28 14:58:38 -0600

    made block & piece. I like you.

---
+++ src/com/github/utslcwest/tetrisclone/model/Block.java
@@ -0,0 +1,17 @@
+package com.github.utslcwest.tetrisclone.model;
+
+import java.awt.Color;
+
+public class Block {
+	
+	Color color;
+	
+	void setColor (Color color){
+		this.color = color;
+	}
+	
+	void setPosition (Position position){
+		
+	}
+
+}
--- src/com/github/utslcwest/tetrisclone/model/ScoreCounter.java
+++ src/com/github/utslcwest/tetrisclone/model/ScoreCounter.java
@@ -1,19 +1,24 @@
 package com.github.utslcwest.tetrisclone.model;
 
 public class ScoreCounter {
-	
+
 	private int score;
-	
+
 	public ScoreCounter() {
 		score = 0;
 	}
-	
+
 	public int increaseScore(int points) {
 		score += points;
 		return score;
 	}
-	
+
 	public int getScore() {
+		return score;
+	}
+	
+	public int resetScore() {
+		score = 0;
 		return score;
 	}
 
