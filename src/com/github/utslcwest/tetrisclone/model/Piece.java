af2ded5e646995dcdc04dee9c89d21f00c16b8e7
Stakedell5 <Stakedell5@192.168.109.188>
2017-06-28 16:23:18 -0600

    piece and playarea

--- src/com/github/utslcwest/tetrisclone/model/Piece.java
+++ src/com/github/utslcwest/tetrisclone/model/Piece.java
@@ -6,10 +6,13 @@ public class Piece {
 	
 	private List<Block> blocks;
 	private Block pivoter;
+	private PlayArea playArea;
 	
 	public Piece(List<Block> blocks, Block pivoter) {
 		this.blocks = blocks;
 		this.pivoter = pivoter;
 	}
+	
+	
 
 }
---
+++ src/com/github/utslcwest/tetrisclone/model/PlayArea.java
@@ -0,0 +1,13 @@
+package com.github.utslcwest.tetrisclone.model;
+
+import java.util.List;
+
+public class PlayArea {
+	
+	private static final int MAX_X = 9, MAX_Y = 19;
+	private static final int MIN_X = 0, MIN_Y = 0;
+	private Piece activePiece;
+	private List<Block> blocks;
+	
+
+}
