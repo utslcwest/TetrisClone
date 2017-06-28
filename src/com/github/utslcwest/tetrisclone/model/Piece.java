cb77ac2117234659d54c402688db7dd584d0d41b
Stakedell5 <Stakedell5@192.168.109.188>
2017-06-28 15:49:40 -0600

    holy moley

--- src/com/github/utslcwest/tetrisclone/model/Piece.java
+++ src/com/github/utslcwest/tetrisclone/model/Piece.java
@@ -1,5 +1,15 @@
 package com.github.utslcwest.tetrisclone.model;
 
+import java.util.List;
+
 public class Piece {
+	
+	private List<Block> blocks;
+	private Block pivoter;
+	
+	public Piece(List<Block> blocks, Block pivoter) {
+		this.blocks = blocks;
+		this.pivoter = pivoter;
+	}
 
 }
--- src/com/github/utslcwest/tetrisclone/model/PieceType.java
+++ src/com/github/utslcwest/tetrisclone/model/PieceType.java
@@ -1,9 +1,48 @@
 package com.github.utslcwest.tetrisclone.model;
 
-public enum PieceType {
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
-	LINE, FORWARD_L, BACKWARD_L, FORWARD_Z, BACKWARD_Z, SQUARE, HALF_PLUS;
+import com.google.common.collect.Lists;
+
+public enum PieceType {
 	
+	LINE (Color.GREEN, 0, 0, 0, 1, 0, 2, 0, 3), //
+	FORWARD_L (new Color(255, 0, 255), 0, 0, 1, 0, 0, 1, 0, 2), //
+	BACKWARD_L (Color.BLUE, 0, 0, 1, 0, 1, 1, 1, 2), //
+	FORWARD_Z (Color.YELLOW, 1, 2, 1, 1, 0, 1, 0, 0), //
+	BACKWARD_Z (Color.RED, 0, 2, 0, 1, 1, 1, 1, 0), //
+	SQUARE (Color.ORANGE, 0, 0, 0, 1, 1, 0, 1, 1), //
+	HALF_PLUS (Color.CYAN, 0, 0, 0, 1, 0, 2, 1, 1);
 	
+	private final Color color;
+	private final int x1, x2, x3, x4, y1, y2, y3, y4;
 	
+	PieceType(Color color, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+		this.color = color;
+		this.x1 = x1;
+		this.x2 = x2;
+		this.x3 = x3;
+		this.x4 = x4;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.y3 = y3;
+		this.y4 = y4;
+	}
+	
+	private Piece createPiece(PieceType type) {
+		switch (type) {
+		case LINE:
+			List<Block> blocks = Lists.newArrayList();
+			blocks.add(new Block(LINE.color, new Position(LINE.x1, LINE.y1)));
+			blocks.add(new Block(LINE.color, new Position(LINE.x2, LINE.y2)));
+			blocks.add(new Block(LINE.color, new Position(LINE.x3, LINE.y3)));
+			blocks.add(new Block(LINE.color, new Position(LINE.x4, LINE.y4)));
+			Block pivoter = blocks.get(0);
+			break;
+		}
+		return null;
+	}
+
 }
