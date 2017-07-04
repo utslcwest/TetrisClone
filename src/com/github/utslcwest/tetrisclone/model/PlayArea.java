ac5575bbab859e2987cd1c173923ad64b1bbf37a
Stakedell5 <Stakedell5@192.168.109.188>
2017-07-04 16:08:07 -0600

    Initialized blocks in PlayArea

--- src/com/github/utslcwest/tetrisclone/model/PlayArea.java
+++ src/com/github/utslcwest/tetrisclone/model/PlayArea.java
@@ -1,6 +1,9 @@
 package com.github.utslcwest.tetrisclone.model;
 
+import java.awt.Color;
 import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 public class PlayArea {
 	
@@ -8,5 +11,10 @@ public class PlayArea {
 	private static final int MIN_X = 0, MIN_Y = 0;
 	private Piece activePiece;
 	private List<Block> blocks;
+	
+	public PlayArea() {
+		blocks = newArrayList();
+		
+	}
 
 }
