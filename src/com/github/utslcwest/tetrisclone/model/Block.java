dfffcc77ac51de214705b6ebb26c265492708cef
Stakedell5 <Stakedell5@192.168.109.188>
2017-07-04 14:57:32 -0600

    Changed Position from Object to Data Structure

--- src/com/github/utslcwest/tetrisclone/model/Block.java
+++ src/com/github/utslcwest/tetrisclone/model/Block.java
@@ -3,28 +3,28 @@ package com.github.utslcwest.tetrisclone.model;
 import java.awt.Color;
 
 public class Block {
-	
+
 	private Color color;
-	
+
 	private Position position;
-	
-	public Block(Color color, Position position){
+
+	public Block(Color color, Position position) {
 		this.color = color;
 		this.position = position;
-		
+
 	}
-	
-	public void setPosition (Position position){
+
+	public void setPosition(Position position) {
 		this.position = position;
-		
+
 	}
-	
-	public Position getPosition(){
+
+	public Position getPosition() {
 		return position;
-		
+
 	}
-	
-	public Color getColor(){
+
+	public Color getColor() {
 		return color;
 	}
 
