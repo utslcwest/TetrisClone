1e1fe9a3aaba08404385258afce86f438e5a150b
Stakedell5 <Stakedell5@192.168.109.188>
2017-07-04 16:52:16 -0600

    Changed blocks from List to Map in PlayArea

--- src/com/github/utslcwest/tetrisclone/model/PlayArea.java
+++ src/com/github/utslcwest/tetrisclone/model/PlayArea.java
@@ -2,22 +2,23 @@ package com.github.utslcwest.tetrisclone.model;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.util.HashMap;
 import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
+import java.util.Map;
 
 public class PlayArea {
 
 	private static final int MAX_X = 9, MAX_Y = 19;
 	private static final int MIN_X = 0, MIN_Y = 0;
 	private Piece activePiece;
-	private List<Block> blocks;
+	private Map<Point, Block> blocks;
 
 	public PlayArea() {
-		blocks = newArrayList();
+		blocks = new HashMap<Point, Block>();
 		for (int i = MIN_X; i <= MAX_X; i++) {
 			for (int j = MIN_Y; j <= MAX_Y; j++) {
-				blocks.add(new Block(Color.BLACK, new Point(i, j)));
+				Point point = new Point(i, j);
+				blocks.put(point, new Block(Color.BLACK, point));
 			}
 		}
 	}
