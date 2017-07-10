9c94cc1b5ca38aa2cbb70fcd17016c4244def8d6
Stakedell5 <Stakedell5@192.168.109.188>
2017-07-10 15:18:23 -0600

    Started view package

---
+++ src/com/github/utslcwest/tetrisclone/view/TetrisGameView.java
@@ -0,0 +1,11 @@
+package com.github.utslcwest.tetrisclone.view;
+
+import javax.swing.JFrame;
+
+public class TetrisGameView extends JFrame {
+
+	private static final long serialVersionUID = 3823425497177811571L;
+	
+	
+
+}
